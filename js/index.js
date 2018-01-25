ab5718f57e48be54900f5c772841c6b721588c58
FrontlineZS <rkochaniuk@gmail.com>
2018-01-25 13:52:43 +0100

    25-01-2018--responsive navbar with hamburger

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+let hamburger = document.querySelector('#hamburger');
+let mySideNav = document.querySelector('#mySidenav');
 
+hamburger.addEventListener('click', function(event) {
+  event.preventDefault();
+  hamburger.classList.toggle('no-hamburger');
+  mySideNav.classList.toggle('sidenav--open');
+});
