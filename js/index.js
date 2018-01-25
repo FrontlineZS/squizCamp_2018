c536f130bfdab7a883200b79784fe79e7929fa44
Radoslaw Kochaniuk <rkochaniuk@gmail.com>
2018-01-26 00:55:02 +0100

    domowe-porzadki-25.01.2017

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 let hamburger = document.querySelector('#hamburger');
-let mySideNav = document.querySelector('#mySidenav');
+let sideNav = document.querySelector('#mySidenav');
+
 
 hamburger.addEventListener('click', function(event) {
   event.preventDefault();
   hamburger.classList.toggle('no-hamburger');
-  mySideNav.classList.toggle('sidenav--open');
+  sideNav.classList.toggle('sidenav--open');
 });
